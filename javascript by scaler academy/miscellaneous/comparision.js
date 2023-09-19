2889048c1df8fc88dc8fa878af5f959f17a68997
Lucky <luckykashyap3430@gmail.com>
2023-09-19 21:46:46 +0530

    strictly equality operator

--- javascript by scaler academy/miscellaneous/comparision.js
+++ javascript by scaler academy/miscellaneous/comparision.js
@@ -4,5 +4,14 @@ let a = 2;
 
 let b =2;
 
-console.log(a==b);
+
+// == value check
+// ===  value & type both check
+
+
+console.log(a==b);   // true
+
+console.log(2 === '2');         // false
+
+
 
