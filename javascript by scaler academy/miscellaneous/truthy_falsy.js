4d2a63f0750992add150c815943aac98ec087e82
Lucky <luckykashyap3430@gmail.com>
2023-09-19 21:50:33 +0530

    truthy falsy values

---
+++ javascript by scaler academy/miscellaneous/truthy_falsy.js
@@ -0,0 +1,32 @@
+// truthy falsy
+
+// false
+// 0
+// -0
+// "" '' ``
+// null
+// NaN
+// undefined
+
+// truthy
+
+// true
+
+// Infinity
+// -Infinity
+
+// if(0){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+// if(Infinity){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+
