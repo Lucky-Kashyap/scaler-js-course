058857e9ea2af074dc859493dce25555e94546f8
Lucky <luckykashyap3430@gmail.com>
2023-09-19 22:26:05 +0530

    nested object does not work in shallow copy

--- javascript by scaler academy/miscellaneous/shallow_copy.js
+++ javascript by scaler academy/miscellaneous/shallow_copy.js
@@ -2,11 +2,24 @@
 
 let firstName = {
     name:'Adam',
-    age:23
+    age:23,
+    address:{
+        locality:'80 feet road',
+        colony:'gokul',
+        city:'Delhi'
+    }
 }
+
+
+
+firstName.address.city='Bijnor';
+
+
 
 let secondName = {...firstName};
 secondName.name = 'second name';
+
+secondName.address.colony='Badarpur';
 
 console.log('first name');
 console.log(firstName);
