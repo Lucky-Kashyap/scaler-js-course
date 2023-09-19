7c4693fab010734ee1d1b2b65a1e072fe7dc624b
Lucky <luckykashyap3430@gmail.com>
2023-09-19 22:08:48 +0530

    value vs reference type check

--- javascript by scaler academy/miscellaneous/primitive_ref.js
+++ javascript by scaler academy/miscellaneous/primitive_ref.js
@@ -1,11 +1,27 @@
 
 
-let firstPerson = 'Adam';
+// let firstPerson = 'Adam';
+
+// let secondPerson = firstPerson;
+
+// firstPerson = 'Steve';
+
+// console.log(firstPerson);
+// console.log(secondPerson);
+
+
+let firstPerson = {
+    name:'Adam',
+    age:23
+};
 
 let secondPerson = firstPerson;
 
-firstPerson = 'Steve';
+firstPerson.name='Lucky Kashyap';
 
+console.log('first person object');
 console.log(firstPerson);
+console.log('second person object');
 console.log(secondPerson);
+
 
