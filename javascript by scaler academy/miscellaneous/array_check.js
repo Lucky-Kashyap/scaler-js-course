7d852b9229983affd0ddc2e9186a2624179ee744
Lucky <luckykashyap3430@gmail.com>
2023-09-19 23:02:19 +0530

    Array type check using Array.isArray method

---
+++ javascript by scaler academy/miscellaneous/array_check.js
@@ -0,0 +1,10 @@
+let arr = [];
+
+console.log(typeof arr); // object
+
+console.log(Array.isArray(arr)); //true
+
+let a = 12;
+
+console.log(typeof a); // number
+console.log(Array.isArray(a)); // false
