de1acf08d085d5c2b2daf83977f6a5243d46e01f
Lucky <luckykashyap3430@gmail.com>
2023-09-19 22:50:51 +0530

    null undefined notdefined

---
+++ javascript by scaler academy/miscellaneous/null_notdefined_undefined.js
@@ -0,0 +1,35 @@
+// let a = null;
+
+// console.log(a);
+
+// console.log(a);   // a is not definded
+
+// console.log(a);
+// let a;
+
+// console.log(a);
+
+
+
+// console.log(a);
+
+// var a=10;
+
+// let a = 2;
+
+// console.log(a);
+
+let arr=[1,2,3];
+
+// console.log(arr[2]);
+console.log(arr[4]);
+
+// function add(a,b){
+//     let c = a+b;
+
+//     return c;
+// }
+
+// let sum = add(4,5);
+// console.log(sum);
+
