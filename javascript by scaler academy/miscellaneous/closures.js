3803131dfe623506b1b575143267f5b7161459d0
Lucky <luckykashyap3430@gmail.com>
2023-09-19 22:40:43 +0530

    closures with nested functions

---
+++ javascript by scaler academy/miscellaneous/closures.js
@@ -0,0 +1,45 @@
+// closures
+
+// function test(){
+//     let a = 2;
+
+//     function test1(){
+//         console.log(a);
+//     }
+
+//     // test1();
+
+//     return test1;
+// }
+
+// let fun = test();
+
+// fun();
+
+
+function greet(){
+    let name = 'Steve';
+
+    function displayName(){
+        let age = 23;
+
+        console.log(`Hi ${name}`);
+
+        function displayAge(){
+            console.log(`Age : ${age} Name ${name} `);
+        }
+        return displayAge;
+    }
+    return displayName;
+}
+
+
+
+let greetFun = greet();
+
+let greetAge =greetFun();
+
+greetAge();
+
+
+
