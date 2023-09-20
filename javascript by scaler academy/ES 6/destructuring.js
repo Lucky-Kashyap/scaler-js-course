965c95c620a2c9c883bcbc77e9293646103e2517
Lucky <luckykashyap3430@gmail.com>
2023-09-20 16:00:42 +0530

    destructuring arrays

---
+++ javascript by scaler academy/ES 6/destructuring.js
@@ -0,0 +1,15 @@
+// the destructuring assignment systax is a js expression that makes it possible to unpack values
+// from arrays, or properties from objects, into distinct variables.
+
+let arr = ['yellow','red','orange','black','purple'];
+
+let [y,r,o,b,p,e] = arr;
+
+// console.log(y);
+
+
+// console.log(arr[2]);
+
+console.log(y,r,o,b,p,e);
+
+
