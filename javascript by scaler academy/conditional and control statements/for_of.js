556fe484fb7eb9c15c0a07de146305c81d44272a
Lucky <luckykashyap3430@gmail.com>
2023-09-20 15:10:24 +0530

    for of loop

---
+++ javascript by scaler academy/conditional and control statements/for_of.js
@@ -0,0 +1,33 @@
+// The for of statement creates a loop iterating-over iterable objects,
+// including: built-in String,Array,array-like objects like NodeList and also map and set
+
+// let arr = ["yellow", "red", "orange", "blue", "black"];
+
+// for (i of arr) {
+// //   console.log(i);
+// console.log(arr.entries());
+// }
+
+// method - entries - for access indexes
+
+// for([index,color] of arr){
+//     console.log(index);
+// }
+
+// for([index,color] of arr.entries()){
+//     console.log(index ,color);
+// }
+
+// console.log(arr.entries());
+
+// for (key of arr.entries()) {
+//   console.log(key);
+// console.log(arr.key);
+// console.log(arr[key]);
+// }
+
+// var str = "Lucky Kashyap";
+
+// for (key of str) {
+//   console.log(key);
+// }
