b4b12e16b173e88ded66ed9bed49e5cdc279afb3
Lucky <luckykashyap3430@gmail.com>
2023-09-20 16:49:29 +0530

    array methods

---
+++ javascript by scaler academy/basic of Js/array_methods.js
@@ -0,0 +1,20 @@
+var arr = [12, true, "adam", 23, null];
+
+console.log(arr);
+
+// remove last element
+
+// arr.pop();
+
+// add an element
+
+// arr.push("lucky");
+
+// remove first element
+// arr.shift();
+
+// add at first index
+
+arr.unshift('lucky');
+
+console.log(arr);
