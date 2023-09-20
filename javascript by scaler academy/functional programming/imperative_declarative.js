51bd2db869b62ad8c14e69e9c22c3cea04efb830
Lucky <luckykashyap3430@gmail.com>
2023-09-20 17:28:13 +0530

    imperative & declarative approach

---
+++ javascript by scaler academy/functional programming/imperative_declarative.js
@@ -0,0 +1,28 @@
+// we will be given a number and we have to check that if the square of that number
+// is even or not
+
+// const a =12;
+// const a = 11;
+
+// function square(a) {
+//   // console.log(a*a);
+//   if ((a * a) & 1) {
+//     console.log("odd");
+//   } else {
+//     console.log("even");
+//   }
+// }
+
+// square(a);
+
+const checkForSquare = (x)=> (x*x%2 === 0 ?true:false);
+
+
+console.log(checkForSquare(9));
+
+// if(checkForSquare(1)){
+//     console.log('even');
+// }
+// else{
+//     console.log('odd');
+// }
