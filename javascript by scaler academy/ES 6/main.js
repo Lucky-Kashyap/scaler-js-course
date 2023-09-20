3b335ede43243f61bbad85a13754e6301902e211
Lucky <luckykashyap3430@gmail.com>
2023-09-20 16:17:00 +0530

    modules & modularity in js

---
+++ javascript by scaler academy/ES 6/main.js
@@ -0,0 +1,27 @@
+// function add(a, b) {
+//   console.log(a + b);
+// }
+
+// function sub(a, b) {
+//   console.log(a - b);
+// }
+// function mul(a, b) {
+//   console.log(a * b);
+// }
+// function div(a, b) {
+//   console.log(a / b);
+// }
+
+
+// import { addition } from "./calc";
+
+// addition(2,3);
+
+const calculator = require('./calc');
+
+
+calculator.addition(20,10);
+
+
+
+
