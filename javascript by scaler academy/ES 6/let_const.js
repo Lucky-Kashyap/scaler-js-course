546a8c491e9d6e9b68bedf74588f4cabbc55ec55
Lucky <luckykashyap3430@gmail.com>
2023-09-20 15:49:51 +0530

    let vs const keyword

---
+++ javascript by scaler academy/ES 6/let_const.js
@@ -0,0 +1,33 @@
+// let n=10;
+// n=20;
+// console.log(n);
+
+// const num = 40;
+// // num =n;
+// console.log(num);
+
+// console.log(n);
+
+// let n= 10;
+
+// console.log(n);
+
+// const n= 10;
+
+// redclearation not allowed with let & const
+
+// let n =10;
+
+// let n = 10;
+
+// console.log(n);
+
+// if(1){
+//     let a = 12;
+
+//     console.log(a);
+// }
+
+// cannot access outside scope
+
+// console.log(a);
