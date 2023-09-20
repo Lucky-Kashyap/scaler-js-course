3c946a2cd9eaf6b4634fe307cd469e708b02aed2
Lucky <luckykashyap3430@gmail.com>
2023-09-20 14:30:18 +0530

    if else if

--- javascript by scaler academy/conditional and control statements/if_else.js
+++ javascript by scaler academy/conditional and control statements/if_else.js
@@ -1,10 +1,33 @@
 
-var myScore = 92;
+// var myScore = 92;
+// var myScore = 48;
 
-if(myScore>90){
-    console.log("greater than 90");
+// if(myScore>90){
+//     console.log("greater than 90");
+// }
+// else{
+//     console.log('less marks');
+// }
+
+
+// 90 to 100 -A
+
+// 70 to 89 - B
+
+// 50 to 69 - C
+// lower than 50 -  F
+
+var myScore = 56;
+
+if(myScore>=90 && myScore<=100){
+    console.log("Grade A");
+}
+else if(myScore>=70 && myScore<=89){
+    console.log('Grade B');
+}
+else if(myScore>=50 && myScore<=69){
+    console.log('Grade C');
 }
 else{
-    console.log('less marks');
+    console.log('Grade fail');
 }
-
