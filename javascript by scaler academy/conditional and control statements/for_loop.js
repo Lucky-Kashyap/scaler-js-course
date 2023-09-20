f3750c4106c33b4c2e629410517fb2fdd9a04231
Lucky <luckykashyap3430@gmail.com>
2023-09-20 14:41:19 +0530

    for loop

---
+++ javascript by scaler academy/conditional and control statements/for_loop.js
@@ -0,0 +1,29 @@
+// loops are statements that we can use to-control a flow of the program and to do some repeatative task
+
+// for repeating some task
+
+// print number 1 to 10;
+
+// for(let i=1;i<=10;i++){
+//     console.log(i);
+// }
+
+// print table of a number
+
+// var num = 8;
+
+// for(let i=1;i<=10;i++){
+//     console.log(i*num);
+// }
+
+// array
+
+var arr = [1,2,3,4,5,6];
+
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]*arr[i]);
+}
+
+
+
+
