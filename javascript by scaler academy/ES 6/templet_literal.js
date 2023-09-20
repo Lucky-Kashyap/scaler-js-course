a29774a7d29705a5cf638d610880ff882c4a7568
Lucky <luckykashyap3430@gmail.com>
2023-09-20 15:56:05 +0530

    template literal

---
+++ javascript by scaler academy/ES 6/templet_literal.js
@@ -0,0 +1,7 @@
+let name = "Lucky";
+let age = 23;
+let work = "developer";
+
+console.log("I am " + name + " and my age is " + age);
+
+console.log(`I am ${name} and age is ${age}, working as a ${work}`);
