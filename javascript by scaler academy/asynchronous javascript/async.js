174b9e5391426a12f42bb92a9c59e127a7f8fec0
Lucky <luckykashyap3430@gmail.com>
2023-09-22 18:44:15 +0530

    serial execution of async code

--- javascript by scaler academy/asynchronous javascript/async.js
+++ javascript by scaler academy/asynchronous javascript/async.js
@@ -16,11 +16,23 @@ function cb(err, data) {
   }
 
   console.log("File 1 data -> " + data);
+  fs.readFile("f2.txt", cb1);
 }
 
-fs.readFile("f2.txt", cb1);
+// fs.readFile("f2.txt", cb1);
 
 function cb1(err, data) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log("File 1 data -> " + data);
+  fs.readFile("f3.txt", cb2);
+}
+
+// fs.readFile("f3.txt", cb2);
+
+function cb2(err, data) {
   if (err) {
     console.log(err);
   }
