82d5d50b09554fb48821255e68a01beb03c7e6c4
Lucky <luckykashyap3430@gmail.com>
2023-09-22 18:37:24 +0530

    read a file asynchronously

--- javascript by scaler academy/asynchronous javascript/async.js
+++ javascript by scaler academy/asynchronous javascript/async.js
@@ -1,10 +1,31 @@
-const fs  = require('fs');
+const fs = require("fs");
 
-console.log('first line');
+console.log("first line");
 
-let data = fs.readFileSync('f1.txt');
+// let data = fs.readFileSync('f1.txt');
+// let data2 = fs.readFileSync('f2.txt');
 
+// console.log(data2);
+// console.log(data);
 
-console.log(data);
+fs.readFile("f1.txt", cb);
 
-console.log('last line');
+function cb(err, data) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log("File 1 data -> " + data);
+}
+
+fs.readFile("f2.txt", cb1);
+
+function cb1(err, data) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log("File 1 data -> " + data);
+}
+
+console.log("last line");
