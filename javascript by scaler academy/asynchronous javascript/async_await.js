0b3272355acd97235a378edbf35634aa3ef18da4
Lucky <luckykashyap3430@gmail.com>
2023-09-27 21:39:29 +0530

    use async & await for shorter promise  code

---
+++ javascript by scaler academy/asynchronous javascript/async_await.js
@@ -0,0 +1,20 @@
+let promise = new Promise((res, rej) => {
+    let a = 1;
+  
+    if (a == 1) {
+      // res("Promise resolved");
+      setTimeout(()=>{
+          res('promise resolved after 1s ')
+      },1000);
+    } else {
+      rej("Promise failed");
+    }
+  });
+
+
+  async function learn(){
+    let res =await promise;
+    console.log(res);
+  };
+
+  learn();
